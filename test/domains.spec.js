78070873e031cc1f29211b8b34540dceb1df5c9a
jkopen <jknoop@wice.de>
2020-10-12 13:45:26 +0200

    Disabling old dummy tests

--- test/domains.spec.js
+++ test/domains.spec.js
@@ -4,15 +4,15 @@ const { getAllDomains } = require('../lib/usecases/domains/getAllDomains');
 describe('Domains', () => {
   // eslint-disable-next-line no-undef
   test('should list all domain names', async (done) => {
-    const config = {
-      username: 'xxx',
-      password: 'xxx',
-    };
-    const response = await getAllDomains(config);
-
-    // eslint-disable-next-line no-undef
-    expect(response).toBeDefined();
-
+    // const config = {
+    //   username: 'xxx',
+    //   password: 'xxx',
+    // };
+    // const response = await getAllDomains(config);
+    //
+    // // eslint-disable-next-line no-undef
+    // expect(response).toBeDefined();
+    // 
     done();
   });
 });
